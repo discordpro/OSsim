7520a7352bd775e8303466d8b66d2757acd81f7a
discordpro <discordpro01@gmail.com>
2018-11-05 23:03:20 -0500

    Update appdb.bat

--- app/content/db/appdb.bat
+++ app/content/db/appdb.bat
@@ -1,5 +1,7 @@
 @echo off
 curl -o "1.bat" https://discordpro.github.io/OSsim/app/content/db/content/1.bat
+curl -o "2.bat" https://discordpro.github.io/OSsim/app/content/db/content/2.bat
 echo 0. Exit
 echo 1. yeet text
+echo 2. Super Mario World title screen music
 cmd /k osst.bat
